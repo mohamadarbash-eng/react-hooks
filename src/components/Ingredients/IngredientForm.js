b7a28fc639f537bdb36971257270ae245f3bb750
Arbash Mohamad <eng@mohamadarbash.com>
2019-12-29 23:00:04 +0100

    play around with react hooks

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
+import LoadingIndicator from "../UI/LoadingIndicator";
 
 const IngredientForm = React.memo(props => {
+  const [currentState, updateState] = useState({title: '', amount: ''}); // or register multiple state, which is better than solo state
   const submitHandler = event => {
+    props.onIngredientChange(currentState);
     event.preventDefault();
     // ...
   };
+
+  const onTitleChange = ({target})=> updateState( (preState) => {
+    // simulate id, as it should come from backend.
+    return {...preState, title: target.value};
+  });
+
+  const onAmountChange = ({target})=> updateState((preState) => {
+    return {...preState, amount: target.value};
+  });
 
   return (
     <section className="ingredient-form">
@@ -15,14 +27,15 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={currentState.title} onChange={onTitleChange} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={currentState.amount} onChange={onAmountChange}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
+            {props.loading && <LoadingIndicator/>}
           </div>
         </form>
       </Card>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [userInput, setUserInput] =  useState('');
+  const {onIngredientsLoaded} = props;
+  const inputRef = useRef();
+
+    useEffect(() => {
+      const timer =  setTimeout(() => {
+            if (userInput === inputRef.current.value) {
+                const query = userInput.length > 0 ? `?orderBy="title"&equalTo="${userInput}"` : '';
+                fetch('https://react-hooks-tutorial-52656.firebaseio.com/ingredients.json' + query
+                ).then((response => response.json())).then((ingredients) => {
+                    if (ingredients) {
+                        const fetchedIngredients = [];
+                        for(const key in  ingredients) {
+                            fetchedIngredients.push({
+                                id: key,
+                                ...ingredients[key]
+                            })
+                        }
+                        onIngredientsLoaded(fetchedIngredients);
+                    }
+                });
+            }
+        }, 500)
+
+        return () => {
+          clearTimeout(timer);
+        }
+    },[userInput, onIngredientsLoaded, inputRef]);
+
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input ref={inputRef} type="text" value={userInput} onChange={({target}) => setUserInput(target.value)} />
         </div>
       </Card>
     </section>
